43e9446078afe79ea406cb8a39ad9dd733423795
Egin Pardewan <eginpardewan@gmail.com>
2023-06-09 14:02:38 +0700

    memasukan circularlinkedlist kedalam addNode

--- exercise 3 algoritma/exercise 3 algoritma.cpp
+++ exercise 3 algoritma/exercise 3 algoritma.cpp
@@ -12,5 +12,26 @@ public:
 class CircularLinkedList {
 private:
 	Node* Last;
+public:
+	CircularLinkedList() {
+		LAST = NULL;
+	}
+	void addNode();
+	bool search(int rillno, Node** previous, Node** current);
+	bool listEmpty();
+	bool delNode();
+	void traverse();
 };
+
+void CircularLinkedList::addNode() {
+	int nim;
+	string na;
+	cout << "\nEnter the rollnumber of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name of the student: ";
+	cin >> na;
+	Node* newNode = new Node();
+	newNode->rollNumber = nim;
+	newNode->name = na;
+}
 
