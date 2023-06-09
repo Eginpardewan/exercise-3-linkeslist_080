04d4defe20d20f178d9e5db905cf71c31d5a5350
Egin Pardewan <eginpardewan@gmail.com>
2023-06-09 14:51:53 +0700

    menjalankan circularlinkedlist

--- exercise 3 algoritma/exercise 3 algoritma.cpp
+++ exercise 3 algoritma/exercise 3 algoritma.cpp
@@ -99,3 +99,40 @@ void CircularLinkedList::traverse() {
 	}
 }
 
+int main() {
+	CircularLinkedList obj;
+	while (true) {
+		try {
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. View all the records in the list" << endl;
+			cout << "4. Exit" << endl;
+			cout << "\nEnter your choice (1-5): ";
+			char ch;
+			cin >> ch;
+			switch (ch) {
+			case '1': {
+			}
+			case '2': {
+			}
+			case '3': {
+				obj.traverse();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "Invalid option" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << e.what() << endl;
+		}
+	}
+	return 0;
+}
+
