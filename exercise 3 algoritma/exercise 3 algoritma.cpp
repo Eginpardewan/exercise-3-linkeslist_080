97db2048cc53ba9dee7046e65c27516c40691b8a
Egin Pardewan <eginpardewan@gmail.com>
2023-06-09 14:07:09 +0700

    memasukan circularlinkedlist kedalam pencarian dio dan arya

--- exercise 3 algoritma/exercise 3 algoritma.cpp
+++ exercise 3 algoritma/exercise 3 algoritma.cpp
@@ -35,3 +35,35 @@ void CircularLinkedList::addNode() {
 	newNode->name = na;
 }
 
+/*insert a node in the beginning of a double - linked list*/
+if (LAST == NULL || nim <= LAST->rollNumber) {
+	if (LAST != NULL && nim == LAST->rollNumber) {
+		cout << "\nDuplicate number not allowed" << endl;
+		return;
+	}
+	newNode->next = LAST; //step 3
+	if (LAST != NULL)
+		LAST->rollNumber = newNode; //step 4
+	newNode->rollNumber = NULL; //step 5
+	LAST = newNode; //step 6
+	return;
+}
+
+bool CircularLinkedList::search(int rollno, Node** Dio, Node** Arya) {
+	*Dio = LAST->next;
+	*Arya = LAST->next;
+	while (*Arya != LAST) {
+		if (rollno == (*Arya)->rollNumber) {
+			return true;
+		} *
+			Dio = *Arya;
+		*Arya = (*Arya)->next;
+	}
+	if (rollno == LAST->rollNumber) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
